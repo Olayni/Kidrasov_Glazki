788ad8fbe99f547202ec04eab87507d025200f53
Rafael <rafakidr@yandex.ru>
2023-11-09 22:06:18 +0500

    lab11

--- Kidrasov_Glazki-save/ServicePage.xaml.cs
+++ Kidrasov_Glazki-save/ServicePage.xaml.cs
@@ -23,10 +23,117 @@ namespace Kidrasov_Glazki_save
         public ServicePage()
         {
             InitializeComponent();
+            var currentServices = Kidrasov_glazkiEntities.GetContext().Agent.ToList();
+
+            AgentListView.ItemsSource = currentServices;
+            ComboFilter.SelectedIndex = 0;
+            ComboType.SelectedIndex = 0;
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
         }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void ComboFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void ComboType_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void AgentListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();   
+        }
+
+        public void UpdateServices()
+        {
+
+            var currentServices = Kidrasov_glazkiEntities.GetContext().Agent.ToList();
+
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentServices = currentServices.Where(p => (p.AgentTypeString == "ЗАО")).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentServices = currentServices.Where(p => (p.AgentTypeString == "МКК")).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentServices = currentServices.Where(p => p.AgentTypeString == "МФО").ToList();
+            }
+
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentServices = currentServices.Where(p => p.AgentTypeString == "ОАО").ToList();
+            }
+
+            if (ComboType.SelectedIndex == 5)
+            {
+                currentServices = currentServices.Where(p => p.AgentTypeString == "ООО").ToList();
+            }
+            if (ComboType.SelectedIndex == 6)
+            {
+                currentServices = currentServices.Where(p => p.AgentTypeString == "ПАО").ToList();
+            }
+
+
+
+            if (ComboFilter.SelectedIndex == 1)
+            {
+                currentServices = currentServices.OrderBy(p => p.Title).ToList();
+            }
+            if (ComboFilter.SelectedIndex == 2)
+            {
+                currentServices = currentServices.OrderByDescending(p => p.Title).ToList();
+            }
+
+            if (ComboFilter.SelectedIndex == 3)
+            {
+                currentServices = currentServices.OrderBy(p => p.Title).ToList();
+            }
+            if (ComboFilter.SelectedIndex == 4)
+            {
+                currentServices = currentServices.OrderByDescending(p => p.Title).ToList();
+            }
+
+            if (ComboFilter.SelectedIndex == 5)
+            {
+                currentServices = currentServices.OrderBy(p => p.Priority).ToList();
+            }
+            if (ComboFilter.SelectedIndex == 6)
+            {
+                currentServices = currentServices.OrderByDescending(p => p.Priority).ToList();
+            }
+            currentServices = currentServices.Where(p => p.Title.ToLower().Contains(Search.Text.ToLower()) ||
+            p.Phone.Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", "").ToLower().Contains(Search.Text.ToLower()) ||
+            p.Email.ToLower().Contains(Search.Text.ToLower())).ToList();
+            AgentListView.ItemsSource = currentServices;
+        }
+
+        private void Search_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
     }
 }
+    
+
+
+
